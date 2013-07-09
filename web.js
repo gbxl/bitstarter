c2e3c360f0d989570364b2a4e53077fd4d688634
Ubuntu <ubuntu@ip-172-31-1-7.us-west-2.compute.internal>
2013-07-09 19:11:52 +0000

    update web

--- web.js
+++ web.js
@@ -1,21 +1,13 @@
 var express = require('express');
-var Iconv = require('iconv').Iconv;
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(readFileSync_encoding('index.html', "string"));
+  response.send(fs.readFileSync('index.html').toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-function readFileSync_encoding(filename, encoding) {
-    var content = fs.readFileSync(filename);
-    var iconv = new Iconv(encoding, 'UTF-8');
-    var buffer = iconv.convert(content);
-    return buffer.toString('utf8');
-}
